e4fc608be57b8c586128ba4d43344565ea2d4005
KeerthanaBalasubramani <40912213+KeerthanaBalasubramani@users.noreply.github.com>
2018-07-11 15:11:08 +0530

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Day
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String a=sc.next();
+String b=sc.next();
+if(a.equals("Saturday")||a.equals("Sunday"))
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+if(b.equals("Saturday")||b.equals("Sunday"))
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
+
